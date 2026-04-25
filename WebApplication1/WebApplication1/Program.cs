1ec00e69908381b15ac69ca823ec6b3b6450fe72
Inga Gadomska-Nguyen <s33141@pjwstk.edu.pl>
2026-04-25 23:14:29 +0200

    modyfikacja Program.cs

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,3 +1,5 @@
+using WebApplication1.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<AppointmentService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(opt => opt.SwaggerEndpoint("/openapi/v1.json", "V1"));
 }
 
 app.UseHttpsRedirection();
