a48bdee6f9ee1b4a705a4f8c9d59cf0867c67b16
Inga Gadomska-Nguyen <s33141@pjwstk.edu.pl>
2026-04-25 22:41:37 +0200

    modyfikacja klasy AppointmentDetailsDto

--- WebApplication1/WebApplication1/DTO/AppointmentDetailsDto.cs
+++ WebApplication1/WebApplication1/DTO/AppointmentDetailsDto.cs
@@ -3,9 +3,11 @@
 public class AppointmentDetailsDto
 {
     public int Id { get; set; }
-    public string PatientFullName { get; set; }
+    public string PatientFirstName { get; set; }
+    public string PatientLastName { get; set; }
     public DateTime PatientBirthDate { get; set; }
-    public int DoctorsFullName { get; set; }
+    public string DoctorsFirstName { get; set; }
+    public string DoctorsLastName { get; set; }
     public DateTime AppointmentDate { get; set; }
     public string Status { get; set; }
     public string Reason { get; set; }
